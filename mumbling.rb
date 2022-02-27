fb850ddc5ef4e30a4aa1364bd2755355f034cbec
RaccoonHowell <rac777blue@hotmail.com>
2022-02-27 19:16:50 +0000

    passed test for returning 'Abbccc' when given 'abc' and deleted previous test as now redundant

--- mumbling.rb
+++ mumbling.rb
@@ -2,10 +2,10 @@ class Mumbling
     def mumble_letters string
         new_string = []
 
-        string.each_with_index do |char, index|
+        string.split("").each_with_index do |char, index|
             new_string << char * (index + 1)
         end
 
-        string.capitalize
+        new_string.join.capitalize
     end
 end
