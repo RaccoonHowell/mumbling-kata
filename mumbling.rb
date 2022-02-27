83a503ccfb6884021df3597a18dcd1eca72fea54
RaccoonHowell <rac777blue@hotmail.com>
2022-02-27 19:12:06 +0000

    failed test for returning 'Abbccc' when given 'abc'

--- mumbling.rb
+++ mumbling.rb
@@ -1,5 +1,11 @@
 class Mumbling
     def mumble_letters string
+        new_string = []
+
+        string.each_with_index do |char, index|
+            new_string << char * (index + 1)
+        end
+
         string.capitalize
     end
 end
