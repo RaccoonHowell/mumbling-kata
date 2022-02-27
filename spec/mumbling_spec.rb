8c26656d533155eafd0a9c43ede253ec388e41b8
RaccoonHowell <rac777blue@hotmail.com>
2022-02-27 19:24:59 +0000

    passed test for returning 'A-Bb-Ccc-Dddd' when given 'abcd' and deleted previous test

--- spec/mumbling_spec.rb
+++ spec/mumbling_spec.rb
@@ -5,13 +5,8 @@ describe Mumbling do
         @mumbling = described_class.new
     end
 
-    it "can return 'ABbCcc' when given 'abc'" do
-        result = @mumbling.mumble_letters("abc")
-        expect(result).to eq("ABbCcc")
-    end
-
-    it "can return 'A-Bb-Ccc-Dddd' when given 'abc'" do
-        result = @mumbling.mumble_letters("abc")
+    it "can return 'A-Bb-Ccc-Dddd' when given 'abcd'" do
+        result = @mumbling.mumble_letters("abcd")
         expect(result).to eq("A-Bb-Ccc-Dddd")
     end
 end
