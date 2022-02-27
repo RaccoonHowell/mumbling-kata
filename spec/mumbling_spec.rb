9c4ff39bb37aafe7b07bb40a451bcbc27acbe1d9
RaccoonHowell <rac777blue@hotmail.com>
2022-02-27 18:53:28 +0000

    failed first test for returning a string

--- spec/mumbling_spec.rb
+++ spec/mumbling_spec.rb
@@ -1,1 +1,12 @@
 require "./mumbling"
+
+describe Mumbling do
+    before do
+        @mumbling = described_class.new
+    end
+
+    it "can return a string" do
+        result = @mumbling.mumble_letters
+        expect(result).to eq('hi')
+    end
+end
